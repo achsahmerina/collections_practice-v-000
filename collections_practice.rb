1389a7129c3e26ce359782943ce111ebc8bdf1be
Achsah Abraham <achsahmp@gmail.com>
2018-09-29 22:05:08 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -11,4 +11,6 @@ def sort_array_char_count(array)
   array.sort {|cat, horses| cat.length <=> horses.length}
 end
 
-def swap_elements(array_)
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+end
